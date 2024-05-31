157f45f0f361b6e01c61b330114d7b42c02a7a87
Daiki48 <daiki48.engineer@gmail.com>
2024-05-31 20:52:48 +0900

    :zap: Update sakurajima dinner theme.

---
+++ lua/sakurajima/colors.lua
@@ -0,0 +1,48 @@
+local colors = {}
+
+-- Status color
+colors.none = "NONE"
+colors.cleared = "cleared"
+colors.error = "#8f3231"
+colors.warn = "#C7A252"
+colors.info = "#CEB4A8"
+colors.hint = "#717375"
+
+-- style
+colors.bold = "bold"
+colors.underline = "underline"
+colors.undercurl = "undercurl"
+
+-- Using color
+colors.black = "#22272e"
+colors.black_blue = "#1D3A64"
+
+colors.white = "#ebdbb2"
+colors.dark_white = "#8b9aaa"
+
+colors.dark_gray = "#7D7D7D"
+colors.light_gray = "#989B9D"
+colors.winter_gray = "#2D333B"
+colors.inactivegray = "#7c6f64"
+
+colors.green = "#3DA163"
+colors.dark_green = "#658D50"
+
+colors.blue = "#8395A5"
+colors.dark_blue = "#4F6981"
+colors.light_blue = "#82ade0"
+
+colors.dark_red = "#A77169"
+
+colors.orange = "#E38D2C"
+colors.dark_orange = "#97812C"
+
+colors.yellow = "#E3D92C"
+colors.dark_yellow = "#B3A278"
+colors.dark_yellow_green = "#A7B383"
+
+colors.cyan = "#2BB6BA"
+colors.dark_cyan = "#3B7B7D"
+colors.light_cyan = "#5F9D9C"
+
+return colors
