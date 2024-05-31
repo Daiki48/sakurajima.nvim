68c2f920afa3afa4ec663bf8310fe377bf8d3a03
Daiki48 <daiki48.engineer@gmail.com>
2024-05-31 20:37:06 +0900

    :recycle: Update

--- lua/sakurajima/init.lua
+++ lua/sakurajima/init.lua
@@ -1,31 +1,27 @@
--- local highlight = require("sakurajima.highlight")
-
 local sakurajima_dinner = require("sakurajima.themes.dinner")
 
 local M = {}
 
 function M.setup()
-	vim.o.background = 'dark'
+	vim.o.background = "dark"
 
-	if not vim.fn.has('gui_running') and vim.o.t_Co < 256 then
+	if not vim.fn.has("gui_running") and vim.o.t_Co < 256 then
 		return
 	end
 
-	vim.cmd('highlight clear')
-	if vim.fn.exists('syntax_on') then
-		vim.cmd('syntax reset')
+	vim.cmd("highlight clear")
+	if vim.fn.exists("syntax_on") then
+		vim.cmd("syntax reset")
 	end
 
-	vim.g.colors_name = 'sakurajima'
+	vim.g.colors_name = "sakurajima"
 
-	if not vim.fn.has('nvim') then
+	if not vim.fn.has("nvim") then
 		vim.o.cursorline = true
-		vim.o.cursorlineopt = 'number'
+		vim.o.cursorlineopt = "number"
 	end
 
-	-- sakurajima_dinner().setup()
 	sakurajima_dinner.setup()
-
 end
 
 return M
